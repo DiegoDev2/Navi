3b210aa373824e5e350a2244a605860333f93b6d
CodeDiego15 <andrebermudezdiego@gmail.com>
2024-08-30 04:08:01 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+# Navi: Tu Explorador de Archivos Personalizado 
+
+**Navega por tu sistema de archivos de forma rápida y sencilla.**
+
+## ¿Qué es Navi?
+Navi es una herramienta de línea de comandos diseñada para ofrecer una experiencia de navegación de archivos intuitiva y eficiente. Con su interfaz minimalista, te permite explorar tus archivos y directorios de manera rápida y sencilla.
+
+## Características Clave:
+* **Minimalista:** Una interfaz limpia y enfocada en lo esencial.
+* **Multiplataforma:** Funciona en macOS, Linux y Windows.
+* **Intuitivo:** Navega con las flechas y abre archivos con Enter.
+* **Versátil:** Soporta una amplia variedad de archivos y directorios.
+
